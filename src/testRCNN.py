2611163f03acce19b389cefe508e3acee0199c20
coldmayo <wallachm@msu.edu>
2023-11-15 13:50:14 -0500

    testRCNN.py now shows # of tracks, saves to results.txt

--- src/testRCNN.py
+++ src/testRCNN.py
@@ -25,12 +25,14 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_F
 cfg.MODEL.DEVICE = "cpu"
 cfg.MODEL.WEIGHTS = "MMBCReco/src/output/model_final.pth"
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85   # set the testing threshold for this model
 predictor = DefaultPredictor(cfg)
 
 path = "MMBCReco/src/valData"
 savePath = "MMBCReco/src/results"
 j = 0
+inText = []
+
 for i in os.listdir(path):
     im = cv2.imread(path+"/"+i)
     output = predictor(im)
@@ -39,6 +41,19 @@ for i in os.listdir(path):
         instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
     )
     v = v.draw_instance_predictions(output["instances"].to("cpu"))
+    print("Predicted # of tracks: "+str(len(output["instances"].to("cpu"))))
+    inText.append(str(len(output["instances"].to("cpu")))+", "+ str(j))
     plt.imshow(v.get_image()[:, :, ::-1])
     plt.savefig(savePath+"/pic"+str(j)+".png")
+    print("image "+str(j)+" done\n")
     j = j+1
+
+# save number of tracks for each image into text file
+
+with open(savePath+'/results.txt', 'w') as f:
+    for i in range(len(inText)):
+        f.write(inText[i])
+        f.write('\n')
+
+
+print("check results.txt")
